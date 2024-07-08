5803214dd227d6d08f030da1874b82984e760ccd
Luan Hott <luanhottalt@gmail.com>
2024-07-08 19:07:37 -0300

    Limpeza dos dados

--- notebooks/coin_cleanup.py
+++ notebooks/coin_cleanup.py
@@ -4,11 +4,13 @@ import pandas as pd
 import os
 
 input_folder = "../data/Coin_Raw/"
-output_folder = "../data/Coin_Clean/"
 start_date = '2020-01-01'
 end_date = '2020-12-31'
+
 #%%
 csv_files = [f for f in os.listdir(input_folder) if f.endswith('.csv')]
+
+df_list = []
 
 for csv_file in csv_files:
 
@@ -19,8 +21,19 @@ for csv_file in csv_files:
     coins_filtered = df_coins[(df_coins['Date'] >= start_date) & (df_coins['Date'] <= end_date)]
 
     coins_filtered.reset_index(inplace=True, drop=True)
-    
-    output_file = os.path.join(output_folder, csv_file.replace('.csv', '_clean.csv'))
-    coins_filtered.to_csv(output_file, index=False)
 
+    df_list.append(coins_filtered)
+   
+# %%
+coins_final = pd.concat(df_list, ignore_index=True)
+
+coins_final[['High', 'Volume', 'Marketcap']] = coins_final[['High', 'Volume', 'Marketcap']].round(4)
+pd.set_option('display.float_format', '{:.4f}'.format)
+
+coins_final['Date'] = pd.to_datetime(coins_final['Date'], errors='coerce').dt.date    
+coins_final['Date'] = pd.to_datetime(coins_final['Date'].astype(str), errors='coerce')
+# %%
+coins_final
+# %%
+coins_final.to_csv("../data/Coin_Clean/Coin_Clean.csv", index=False)
 # %%
